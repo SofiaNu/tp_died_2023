7ca4d4cd1f276b201fb5ab229ca86ae8e0e2c6c2
Sofia N <sofia.30.10.99@gmail.com>
2023-09-10 20:39:12 -0300

    front tabla pagerank

--- tp_died_2023/src/main/java/gui/Inicio.java
+++ tp_died_2023/src/main/java/gui/Inicio.java
@@ -1,16 +1,17 @@
 package gui;
 
-import java.awt.EventQueue;
+import clases.Sucursal;
+
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.*;
 import javax.swing.border.EmptyBorder;
-import javax.swing.JButton;
-import java.awt.FlowLayout;
-import java.awt.Font;
+import javax.swing.table.DefaultTableModel;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Inicio extends JFrame{
 
@@ -65,6 +66,64 @@ public class Inicio extends JFrame{
 			}
 		});
 		contentPane.add(btnOrdenes);
+
+		JButton btnPageRank = new JButton("Page Rank");
+		btnPageRank.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				showPageRank();
+			}
+		});
+		contentPane.add(btnPageRank);
+
+		JButton btnFlujo = new JButton("Flujo Maximo");
+		btnFlujo.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				showVentanaFlujo();
+			}
+		});
+		contentPane.add(btnFlujo);
+	}
+
+	public void showVentanaFlujo(){
+
+	};
+
+	public void showPageRank(){
+
+		JFrame resultadoFrame = new JFrame("Page Rank:");
+		resultadoFrame.setSize(500, 300);
+		resultadoFrame.setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		JPanel panel = new JPanel(new FlowLayout());
+
+		String[] columnNames = {"Id", "Nombre", "Ranking"};
+		JTable tablaResultados = new JTable(new DefaultTableModel());
+
+		DefaultTableModel model = (DefaultTableModel) tablaResultados.getModel();
+		model.setColumnIdentifiers(columnNames);
+
+		List<Sucursal> sucursales = new ArrayList<>(); //ACA VA LA LISTA
+
+		if(sucursales != null) {
+			for (Sucursal s : sucursales) {
+				String[] fila = {String.valueOf(s.getId()), s.getNombre(), String.valueOf(s.getHoraApertura())};
+				model.addRow(fila);
+			}
+		}
+		JScrollPane contenedorTabla = new JScrollPane(tablaResultados); //Sin esto no se muestra el nombre de las columnas
+		int maxVisibleRows = 8;
+		int rowHeight = tablaResultados.getRowHeight();
+		int headerHeight = tablaResultados.getTableHeader().getPreferredSize().height;
+		Dimension preferredSize = new Dimension(contenedorTabla.getPreferredSize().width,
+				maxVisibleRows * rowHeight + headerHeight);
+		contenedorTabla.setPreferredSize(preferredSize);
+		panel.add(contenedorTabla);
+
+		resultadoFrame.add(panel);
+		resultadoFrame.setVisible(true);
+
+
 	}
 
 
